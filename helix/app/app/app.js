76d7bedc3eb8dcfa5440c60ad5d2b20826d22f6c
faderz <58748338+faderzz@users.noreply.github.com>
2023-07-16 20:39:59 +0100

    Works!!

--- helix/app/app/app.js
+++ helix/app/app/app.js
@@ -1,29 +1,67 @@
+'use client'
+import React, { useState } from 'react';
 import Image from 'next/image'
 import axios from 'axios'
 
 export default function App() {
-  var userId
-  var apiKey
+  const [userId, setUserId] = useState('');
+  const [apiKey, setApiKey] = useState('');
+  const [isPopupVisible, setPopupVisible] = useState(false);
 
-  async function prompt(text, user_id) {
-
-    const response = await axios.post('https://api.carterlabs.ai/chat', {
-      text,
-      key: '457771a6-17e8-4ae7-873e-6d46b42c31fe',
-      user_id,
+  async function prompt(textPrompt) {
+    const data = {
+      text: textPrompt,
+      key: apiKey,
+      user_id: userId,
       speak: false,
-    }, {
+    };
+    
+    fetch('https://api.carterlabs.ai/chat', {
+      method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-    });
-  
-    return response.data.response;
+      body: JSON.stringify(data),
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log('Input:', data);
+        console.log('Output Text:', data.output.text);
+        document.getElementById('text').textContent = data.output.text;
+    
+        data.forced_behaviours.forEach(fb => {
+          console.log('Forced Behaviour:', fb.name);
+        });
+      })
+      .catch(error => {
+        console.error(error);
+      });
+    return data.output;
   }
   
+  function handleSettingsClick() {
+    setPopupVisible(!isPopupVisible);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    setUserId(event.target.elements.userId.value);
+    setApiKey(event.target.elements.apiKey.value);
+    setPopupVisible(false);
+  }
+  
+  async function handleKeyDown(event) {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      const text = event.target.value;
+      const message = await prompt(text);
+      console.log(message);
+    }
+  }
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/fittext.js/1.2.0/jquery.fittext.min.js"></script>
       <div data-tauri-drag-region class="titlebar">
         <div class="titlebar-button" id="titlebar-minimize">
           <img src="https://api.iconify.design/mdi:window-minimize.svg?color=white" alt="minimize" />
@@ -50,14 +88,34 @@ export default function App() {
 
         {/* Input box */}
         <div id="inputmessage">
-          <input type="text" id="input" placeholder="Enter your message..." />
+          <input type="text" id="input" placeholder="Enter your message..." onKeyDown={handleKeyDown}/>
         </div>
 
         {/* Settings cog wheel icon */}
         <div class="flex flex-col items-center justify-center" id="settings">
-          <img src="https://api.iconify.design/mdi:cog.svg?color=white" alt="settings"/>
+          <img src="https://api.iconify.design/mdi:cog.svg?color=white" alt="settings" onClick={handleSettingsClick}/>
         </div>
       </div>
+
+      {/* Popup */}
+      {isPopupVisible && (
+        <div className="popup">
+          <form onSubmit={handleSubmit}>
+            
+            <div class="popupitem">
+            <label htmlFor="apiKey">API Key:</label>
+            <input type="text" id="apiKey" name="apiKey" required />
+            <br />
+            </div>
+            <div class="popupitem">
+            <label htmlFor="userId">User ID:</label>
+            <input type="text" id="userId" name="userId" required />
+            <br />
+            </div>
+            <button type="submit">Save</button>
+          </form>
+        </div>
+      )}
     </main>
   )
 }
