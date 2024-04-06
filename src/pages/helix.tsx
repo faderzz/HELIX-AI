e498a5163a79f06d76db64c9498ff60934c82312
faderz <58748338+faderzz@users.noreply.github.com>
2024-04-06 02:46:15 +0100

    Create helix.tsx

---
+++ src/pages/helix.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Navbar from '~/components/navbar'
+
+
+
+export default function profile() {
+  return (
+    <main className="flex flex-col min-h-screen bg-black">
+      <div className="container  gap-12 px-4 py-16">
+        <Navbar />
+        
+      </div>
+      <div className="flex justify-center items-center">
+        {/* Create a circle with tailwind classes */}
+        
+      </div>
+    </main>
+  );
+}
